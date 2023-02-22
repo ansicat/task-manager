d747350f1cfa9d28e558ed0ff8f1871d2862104f
AnDy <ansmbox@gmail.com>
2023-02-23 01:35:03 +0200

    Regiser models of app 'task_manager' for site administration

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task_manager.models import Task, Project, Worker, TaskType, Position
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("is_completed",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("is_completed",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+
+admin.site.unregister(Group)
