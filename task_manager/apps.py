462f12f1f4d8c3f4815c285da4d03374ef77049a
AnDy <ansmbox@gmail.com>
2023-02-24 01:07:31 +0200

    Add modified side HTML framework and page templates for site

--- task_manager/apps.py
+++ task_manager/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class TaskManagerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "task_manager"
+
+
+def cfg_assets_root(request):
+    return {"ASSETS_ROOT": settings.ASSETS_ROOT}
