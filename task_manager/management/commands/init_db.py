57199fb6d377d41870e1a53ee45d5a4a0de6a792
AnDy <ansmbox@gmail.com>
2023-03-09 01:08:09 +0200

    Add command 'init_db' for manage.py

---
+++ task_manager/management/commands/init_db.py
@@ -0,0 +1,8 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        call_command("migrate")
+        call_command("loaddata", "task_manager_db_data.json")
