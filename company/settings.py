ad7f206ed98e3e53ff620de43f1f89d51ccb3a6e
AnDy <ansmbox@gmail.com>
2023-03-08 15:56:51 +0200

    Delete env variable DJANGO_DEBUG from .env_sample

--- company/settings.py
+++ company/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DJANGO_DEBUG"] != "False"
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
